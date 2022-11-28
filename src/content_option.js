a2b81b0ee3438c18bae60190706a65f55a90272a
Egbetimmy <egbetiimmy@gmail.com>
2022-11-28 20:33:33 +0100

    First commit

--- src/content_option.js
+++ src/content_option.js
@@ -1,38 +1,33 @@
-const logotext = "JOHN";
+const logotext = "ET";
 const meta = {
-    title: "John Doe",
-    description: "I’m John Doe data scientist _ Full stack devloper,currently working in Berlin",
+    title: "Egbe Timmy",
+    description: "I’m Egbe Timmy data scientist _ Python devloper",
 };
 
 const introdata = {
-    title: "I’m John Doe",
+    title: "I’m Egbe Timmy",
     animated: {
-        first: "I love coding",
-        second: "I code cool websites",
-        third: "I develop mobile apps",
+        first: "Geophysicist",
+        second: "Data Analyst",
+        third: "Python Developer",
     },
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum",
-    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+    description: "In no particular order",
+    your_img_url: "https://res.cloudinary.com/dypv9yccn/image/upload/v1669366649/portfolio/pic1_i9stex.jpg",
+    img2: "https://res.cloudinary.com/dypv9yccn/image/upload/v1669366649/portfolio/pic2_pxyksl.jpg",
+    img3: "https://res.cloudinary.com/dypv9yccn/image/upload/v1669366648/portfolio/ET_whijfz.png",
 };
 
 const dataabout = {
-    title: "abit about my self",
-    aboutme: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent quis dolor id ligula semper elementum feugiat pretium nulla. Nunc non commodo dolor. Nunc mollis dignissim facilisis. Morbi ut magna ultricies.",
+    title: "A bit about myself",
+    aboutme: "Professional data analyst with more than 2 years of experience gathering, organizing, analyzing, and distributing various kinds of statistical data. Active presenter and assured communicator with the capacity to disseminate knowledge in a way that is useful to end users. creative in coming up with fixes and deciding on changes to ensure the best possible use of organizational data.",
 };
-const worktimeline = [{
-        jobtitle: "Designer of week",
-        where: "YAdfi",
-        date: "2020",
-    },
+const workexperience = [
     {
-        jobtitle: "Designer of week",
-        where: "Jamalya",
-        date: "2019",
-    },
-    {
-        jobtitle: "Designer of week",
-        where: "ALquds",
-        date: "2019",
+        role: "Data Analyst",
+        location: "Warri, Nigeria",
+        company: "FriendZone.",
+        timeline: {"from": "August 2021", "to": "Present"},
+        summary: "• I providing technical expertise in data storage structures, data mining, and data cleansing; \n• Using statistical tools, I identify, analyze, and interpret patterns and trends in complex data. \n• sets that could be helpful for the diagnosis and prediction; \n• I work with programmers and management heads to identify process improvements, opportunities, propose system modifications, and devise data governance strategies; \n• I prepare final analysis reports for the stakeholders to understand the data-analysis steps enabling them to take important decisions based on various facts and trends. \n• I implement visual elements that users see and interact with in a web application.",
     },
 ];
 
@@ -40,42 +35,59 @@ const skills = [{
         name: "Python",
         value: 90,
     },
+    
     {
-        name: "Djano",
+        name: "Flask",
         value: 85,
     },
     {
-        name: "Javascript",
+        name: "Excel",
         value: 80,
+    },
+    {
+        name: "Javascript",
+        value: 70,
     },
     {
         name: "React",
         value: 60,
     },
     {
-        name: "Jquery",
+        name: "SQL",
         value: 85,
     },
 ];
 
-const services = [{
-        title: "UI & UX Design",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+const Education = [{
+        degree: "Full-Stack Developer Nanodegree",
+        school: "Udacity, Inc.",
+        timeline: {"from": "Aug 2022", "to": "Nov 2022"},
+        summary: "This nanodegree course extensively taught various technical skills needed to build database-backed APIs and web applications including:\n\n\t• SQL and Data Modelling for the Web\n\t• API Development and Documentation\n\t• Identity and Access Management\n\t• Server Deployment, Containerization and Testing",
     },
     {
-        title: "Mobile Apps",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+        degree: "Google Data Analytics",
+        school: "Coursera",
+        timeline: {"from": "Aug 2022", "to": "Oct 2022"},
+        summary: "Designed for learners with little to no Data Analytics experience:\n\t•Data Analysis with R.\n\t•Analyze Data using spreadsheets & SQL.\n\t•Apply SQL functions for cleaning and transforming data",
     },
     {
-        title: "Wordpress Design",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+        degree: "AWS Machine Learning Foundations",
+        school: "Udacity, Inc.",
+        timeline: {"from": "Jun 2022", "to": "Nov 2022"},
+        summary: "This nanodegree course  teaches developers of all skill levels machine learning concepts for pursuing a career in ML and provides extensive experience using AWS:\n\n\t• Basic understanding of the machine learning workflow \n\n\t• Basic understanding of linear regression, logistic regression, neural network \n\n\t• Basic understanding of model training and testing processes \n\n\t• Basic knowledge of accuracy, precision, recall, and mean square error (MSE) \n\n\t• SQL and Data Modelling for the Web",
+    },
+    {
+        degree: "BSc. in Geophysics",
+        school: "Federal University of Petroleum Resources, Effurun, Nigeria.",
+        timeline: {"from": "Mar 2014", "to": "Apr 2018"},
+        summary: "• Graduated with Second Class Honours",
     },
 ];
 
 const dataportfolio = [{
-        img: "https://picsum.photos/400/?grayscale",
-        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
+        img: "https://res.cloudinary.com/dypv9yccn/image/upload/e_grayscale/v1669622525/portfolio/1_ZtO60QvYVzN3rU0VuSSU1g_af9twi.jpg",
+        desctiption: "Data cleaning with Python",
+        link: "https://link.medium.com/zLln2XkwNub",
     },
     {
         img: "https://picsum.photos/400/800/?grayscale",
@@ -88,17 +100,12 @@ const dataportfolio = [{
         link: "#",
     },
     {
-        img: "https://picsum.photos/400/600/?grayscale",
-        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
+        img: "https://res.cloudinary.com/dypv9yccn/image/upload/v1669623868/portfolio/Fyuur_hgpsma.jpg",
+        desctiption: "Fyuur",
+        link: "https://github.com/Egbetimmy/Fyuur-musical-app",
     },
     {
         img: "https://picsum.photos/400/300/?grayscale",
-        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/700/?grayscale",
         desctiption: "The wisdom of life consists in the elimination of non-essentials.",
         link: "#",
     },
@@ -108,6 +115,12 @@ const dataportfolio = [{
         desctiption: "The wisdom of life consists in the elimination of non-essentials.",
         link: "#",
     },
+    
+    {
+        img: "https://res.cloudinary.com/dypv9yccn/image/upload/e_grayscale/v1669625125/portfolio/Trivia_API_hqdtij.png",
+        desctiption: "Trivia API",
+        link: "https://github.com/Egbetimmy/Trivia-Api",
+    },
     {
         img: "https://picsum.photos/400/300/?grayscale",
         desctiption: "The wisdom of life consists in the elimination of non-essentials.",
@@ -119,26 +132,26 @@ const dataportfolio = [{
         link: "#",
     },
     {
-        img: "https://picsum.photos/400/550/?grayscale",
-        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
+        img: "https://res.cloudinary.com/dypv9yccn/image/upload/e_grayscale/v1669625458/portfolio/cd0157-Server-Deployment-and-Containerization_rpt76q.png",
+        desctiption: "Server-Deployment-and-Containerization",
+        link: "https://github.com/Egbetimmy/cd0157-Server-Deployment-and-Containerization",
+    },
+    {
+        img: "https://res.cloudinary.com/dypv9yccn/image/upload/e_grayscale/v1669625962/portfolio/coffee-shop-full-stack_b7nnwl.jpg",
+        desctiption: "Coffee Shop",
+        link: "https://github.com/Egbetimmy/Coffee-Shop",
     },
     {
         img: "https://picsum.photos/400/?grayscale",
-        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/700/?grayscale",
         desctiption: "The wisdom of life consists in the elimination of non-essentials.",
         link: "#",
     },
 ];
 
 const contactConfig = {
-    YOUR_EMAIL: "name@domain.com",
-    YOUR_FONE: "(555)123-4567",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
+    YOUR_EMAIL: "timmytimeyin@gmail.com",
+    YOUR_FONE: "(234)80-729-67944",
+    description: "I’m always interested in hearing about new projects and opportunities.",
     // creat an emailjs.com account 
     // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
     YOUR_SERVICE_ID: "service_id",
@@ -147,20 +160,17 @@ const contactConfig = {
 };
 
 const socialprofils = {
-    github: "https://github.com",
-    facebook: "https://facebook.com",
-    linkedin: "https://linkedin.com",
-    twitter: "https://twitter.com",
-    youtube: "https://youtube.com",
-    twitch: "https://twitch.com",
+    github: "https://github.com/Egbetimmy",
+    linkedin: "https://www.linkedin.com/in/egbe-timmy-867672171/",
+    twitter: "https://twitter.com/TimmyEgbe",
 };
 export {
     meta,
     dataabout,
     dataportfolio,
-    worktimeline,
+    workexperience,
     skills,
-    services,
+    Education,
     introdata,
     contactConfig,
     socialprofils,
--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -1,14 +1,31 @@
 import React from "react";
+import Fade from "react-reveal/Fade";
 import "./style.css";
 import { Helmet, HelmetProvider } from "react-helmet-async";
 import { Container, Row, Col } from "react-bootstrap";
 import {
   dataabout,
   meta,
-  worktimeline,
+  workexperience,
   skills,
-  services,
+  Education,
 } from "../../content_option";
+import javascript from "../../assets/tech/javascript.svg";
+import git from "../../assets/tech/git.svg";
+import python from "../../assets/tech/python.svg";
+import sql from "../../assets/tech/sql.svg";
+import react from "../../assets/tech/react.svg";
+import nodejs from "../../assets/tech/nodejs.svg";
+import html5 from "../../assets/tech/html5.svg";
+import css3 from "../../assets/tech/css3.svg";
+import flask from "../../assets/tech/flask.svg";
+import excel from "../../assets/tech/excel.png";
+import r from "../../assets/tech/R.png";
+
+function NewlineText(props) {
+  const text = props.text;
+  return text.split('\n').map(str => <p>{str}</p>);
+}
 
 export const About = () => {
   return (
@@ -29,30 +46,56 @@ export const About = () => {
           <Col lg="5">
             <h3 className="color_sec py-4">{dataabout.title}</h3>
           </Col>
-          <Col lg="7" className="d-flex align-items-center">
+          <Col lg="7" className="d-flex align-items-center service_desc">
             <div>
               <p>{dataabout.aboutme}</p>
             </div>
           </Col>
         </Row>
-        <Row className=" sec_sp">
-          <Col lg="5">
-            <h3 className="color_sec py-4">Work Timline</h3>
+        <Row className="sec_sp">
+          <Col lang="5">
+            <h3 className="color_sec py-4">Education</h3>
           </Col>
           <Col lg="7">
-            <table className="table caption-top">
-              <tbody>
-                {worktimeline.map((data, i) => {
+            {Education.map((data, i) => {
+              return (
+                <div className="service_ py-4" key={i}>
+                  <Fade bottom>
+                    <h5 className="service__title">{data.degree}</h5>
+                    <p className="service_desc">{data.school}</p>
+                    <p className="service_desc">
+                      {data.timeline.from} - {data.timeline.to}
+                    </p>
+                    <p className="service_desc">
+                      <NewlineText text={data.summary} />
+                    </p>
+                  </Fade>
+                </div>
+              );
+            })}
+          </Col>
+        </Row>
+        <Row className=" sec_sp">
+          <Col lg="5">
+            <h3 className="color_sec py-4">Work Experience</h3>
+          </Col>
+          <Col lg="7">
+                {workexperience.map((data, i) => {
                   return (
-                    <tr key={i}>
-                      <th scope="row">{data.jobtitle}</th>
-                      <td>{data.where}</td>
-                      <td>{data.date}</td>
-                    </tr>
+                    <div className="service_ py-4" key={i}>
+                      <Fade bottom>
+                        <p className="service_desc">
+                          {data.timeline.from} - {data.timeline.to}
+                        </p>
+                        <h3 className="service__title">{data.role}</h3>
+                        <p className="service_desc">{data.company}</p>
+                        <p className="service_desc">
+                          <NewlineText text={data.summary} />
+                        </p>
+                      </Fade>
+                    </div>
                   );
                 })}
-              </tbody>
-            </table>
           </Col>
         </Row>
         <Row className="sec_sp">
@@ -63,36 +106,98 @@ export const About = () => {
             {skills.map((data, i) => {
               return (
                 <div key={i}>
-                  <h3 className="progress-title">{data.name}</h3>
-                  <div className="progress">
-                    <div
-                      className="progress-bar"
-                      style={{
-                        width: `${data.value}%`,
-                      }}
-                    >
-                      <div className="progress-value">{data.value}%</div>
+                  <Fade bottom>
+                    <h3 className="progress-title">{data.name}</h3>
+                    <div className="progress">
+                      <div
+                        className="progress-bar"
+                        style={{
+                          width: `${data.value}%`,
+                        }}
+                      >
+                        <div className="progress-value">{data.value}%</div>
+                      </div>
                     </div>
-                  </div>
+                  </Fade>
                 </div>
               );
             })}
           </Col>
         </Row>
         <Row className="sec_sp">
-          <Col lang="5">
-            <h3 className="color_sec py-4">services</h3>
-          </Col>
-          <Col lg="7">
-            {services.map((data, i) => {
-              return (
-                <div className="service_ py-4" key={i}>
-                  <h5 className="service__title">{data.title}</h5>
-                  <p className="service_desc">{data.description}</p>
-                </div>
-              );
-            })}
-          </Col>
+          <Col lg="5">
+						<h2 className="color_sec py-4">Tech Stack</h2>
+					</Col>
+					<Col lg="7">
+						{/* 
+						JavaScript,
+						ReactJS,
+						NodeJS,
+						Python,
+						Flask,
+						HTML,
+						CSS,
+						Git,
+						SQL,
+            R,
+            Excel
+						
+						
+						*/}
+							<div className="tech">
+								<img
+									src={javascript}
+									alt="Javascript"
+									title="Javascript"
+									className="tech__logo"
+								/>
+								<img
+									src={react}
+									alt="React JS"
+									title="React JS"
+									className="tech__logo"
+								/>
+								<img
+									src={python}
+									alt="Python"
+									title="Python"
+									className="tech__logo"
+								/>
+								<img
+									src={flask}
+									alt="Flask"
+									title="Flask"
+									className="tech__logo"
+								/>
+								<img
+									src={nodejs}
+									alt="NodeJS"
+									title="NodeJS"
+									className="tech__logo"
+								/>
+								<img src={sql} alt="SQL" title="SQL" className="tech__logo" />
+								<img
+									src={html5}
+									alt="HTML5"
+									title="HTML5"
+									className="tech__logo"
+								/>
+								<img
+									src={css3}
+									alt="CSS3"
+									title="CSS3"
+									className="tech__logo"
+								/>
+								<img src={git} alt="Git" title="Git" className="tech__logo" />
+								<img
+									src={excel}
+									alt="excel"
+									title="Excel"
+									className="tech__logo"
+								/>
+                <img src={r} alt="r" title="R" className="tech__logo" />
+							</div>
+					</Col>
         </Row>
       </Container>
     </HelmetProvider>
