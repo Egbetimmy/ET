7c1d15dbb84b044448c5bc0cb61858436d29c488
Egbetimmy <egbetiimmy@gmail.com>
2022-12-30 21:15:55 +0100

    updates

--- src/content_option.js
+++ src/content_option.js
@@ -1,208 +1,195 @@
 const logotext = "ET";
 const meta = {
-    title: "Egbe Timmy",
-    description: "I’m Egbe Timmy data scientist _ Python devloper",
+  title: "Egbe Timmy",
+  description: "I’m Egbe Timmy data scientist _ Python devloper",
 };
 
 const introdata = {
-    title: "I’m Egbe Timmy",
-    animated: {
-        first: "Geophysicist",
-        second: "Data Analyst",
-        third: "Python Developer",
-    },
-    description: "In no particular order",
-    your_img_url: "https://res.cloudinary.com/dypv9yccn/image/upload/v1669366649/portfolio/pic1_i9stex.jpg",
-    img2: "https://res.cloudinary.com/dypv9yccn/image/upload/v1669366649/portfolio/pic2_pxyksl.jpg",
-    img3: "https://res.cloudinary.com/dypv9yccn/image/upload/v1669366648/portfolio/ET_whijfz.png",
+  title: "I’m Egbe Timmy",
+  animated: {
+    first: "Geophysicist",
+    second: "Data Analyst",
+    third: "Python Developer",
+  },
+  description: "In no particular order",
+  your_img_url: "https://res.cloudinary.com/dypv9yccn/image/upload/v1669366649/portfolio/pic1_i9stex.jpg",
+  img2: "https://res.cloudinary.com/dypv9yccn/image/upload/v1669366649/portfolio/pic2_pxyksl.jpg",
+  img3: "https://res.cloudinary.com/dypv9yccn/image/upload/v1669366648/portfolio/ET_whijfz.png",
 };
 
 const dataabout = {
-    title: "A bit about myself",
-    aboutme: "Professional data analyst with more than 2 years of experience gathering, organizing, analyzing, and distributing various kinds of statistical data. Active presenter and assured communicator with the capacity to disseminate knowledge in a way that is useful to end users. creative in coming up with fixes and deciding on changes to ensure the best possible use of organizational data.",
+  title: "A bit about myself",
+  aboutme: "Professional data analyst with more than 2 years of experience gathering, organizing, analyzing, and distributing various kinds of statistical data. Active presenter and assured communicator with the capacity to disseminate knowledge in a way that is useful to end users. creative in coming up with fixes and deciding on changes to ensure the best possible use of organizational data.",
 };
 const workexperience = [
-    {
-        role: "Data Analyst",
-        location: "Warri, Nigeria",
-        company: "FriendZone.",
-        timeline: {"from": "August 2021", "to": "Present"},
-        summary: "• I providing technical expertise in data storage structures, data mining, and data cleansing; \n• Using statistical tools, I identify, analyze, and interpret patterns and trends in complex data. \n• sets that could be helpful for the diagnosis and prediction; \n• I work with programmers and management heads to identify process improvements, opportunities, propose system modifications, and devise data governance strategies; \n• I prepare final analysis reports for the stakeholders to understand the data-analysis steps enabling them to take important decisions based on various facts and trends. \n• I implement visual elements that users see and interact with in a web application.",
-    },
+  {
+    role: "Data Analyst",
+    location: "Warri, Nigeria",
+    company: "FriendZone.",
+    timeline: { "from": "August 2021", "to": "Present" },
+    summary: "• I providing technical expertise in data storage structures, data mining, and data cleansing; \n• Using statistical tools, I identify, analyze, and interpret patterns and trends in complex data. \n• sets that could be helpful for the diagnosis and prediction; \n• I work with programmers and management heads to identify process improvements, opportunities, propose system modifications, and devise data governance strategies; \n• I prepare final analysis reports for the stakeholders to understand the data-analysis steps enabling them to take important decisions based on various facts and trends. \n• I implement visual elements that users see and interact with in a web application.",
+  },
 ];
 
 const skills = [{
-        name: "Python",
-        value: 90,
-    },
-    
-    {
-        name: "Flask",
-        value: 85,
-    },
-    {
-        name: "Excel",
-        value: 80,
-    },
-    {
-        name: "Javascript",
-        value: 70,
-    },
-    {
-        name: "React",
-        value: 60,
-    },
-    {
-        name: "SQL",
-        value: 85,
-    },
+  name: "Python",
+  value: 90,
+},
+
+{
+  name: "Flask",
+  value: 85,
+},
+{
+  name: "Excel",
+  value: 80,
+},
+{
+  name: "Javascript",
+  value: 70,
+},
+{
+  name: "React",
+  value: 60,
+},
+{
+  name: "SQL",
+  value: 85,
+},
 ];
 
 const Education = [{
-        degree: "Full-Stack Developer Nanodegree",
-        school: "Udacity, Inc.",
-        timeline: {"from": "Aug 2022", "to": "Nov 2022"},
-        summary: "This nanodegree course extensively taught various technical skills needed to build database-backed APIs and web applications including:\n\n\t• SQL and Data Modelling for the Web\n\t• API Development and Documentation\n\t• Identity and Access Management\n\t• Server Deployment, Containerization and Testing",
-    },
-    {
-        degree: "Google Data Analytics",
-        school: "Coursera",
-        timeline: {"from": "Aug 2022", "to": "Oct 2022"},
-        summary: "Designed for learners with little to no Data Analytics experience:\n\t•Data Analysis with R.\n\t•Analyze Data using spreadsheets & SQL.\n\t•Apply SQL functions for cleaning and transforming data",
-    },
-    {
-        degree: "AWS Machine Learning Foundations",
-        school: "Udacity, Inc.",
-        timeline: {"from": "Jun 2022", "to": "Nov 2022"},
-        summary: "This nanodegree course  teaches developers of all skill levels machine learning concepts for pursuing a career in ML and provides extensive experience using AWS:\n\n\t• Basic understanding of the machine learning workflow \n\n\t• Basic understanding of linear regression, logistic regression, neural network \n\n\t• Basic understanding of model training and testing processes \n\n\t• Basic knowledge of accuracy, precision, recall, and mean square error (MSE) \n\n\t• SQL and Data Modelling for the Web",
-    },
-    {
-        degree: "BSc. in Geophysics",
-        school: "Federal University of Petroleum Resources, Effurun, Nigeria.",
-        timeline: {"from": "Mar 2014", "to": "Apr 2018"},
-        summary: "• Graduated with Second Class Honours",
-    },
+  degree: "Full-Stack Developer Nanodegree",
+  school: "Udacity, Inc.",
+  timeline: { "from": "Aug 2022", "to": "Nov 2022" },
+  summary: "This nanodegree course extensively taught various technical skills needed to build database-backed APIs and web applications including:\n\n\t• SQL and Data Modelling for the Web\n\t• API Development and Documentation\n\t• Identity and Access Management\n\t• Server Deployment, Containerization and Testing",
+},
+{
+  degree: "Google Data Analytics",
+  school: "Coursera",
+  timeline: { "from": "Aug 2022", "to": "Oct 2022" },
+  summary: "Designed for learners with little to no Data Analytics experience:\n\t•Data Analysis with R.\n\t•Analyze Data using spreadsheets & SQL.\n\t•Apply SQL functions for cleaning and transforming data",
+},
+{
+  degree: "AWS Machine Learning Foundations",
+  school: "Udacity, Inc.",
+  timeline: { "from": "Jun 2022", "to": "Nov 2022" },
+  summary: "This nanodegree course  teaches developers of all skill levels machine learning concepts for pursuing a career in ML and provides extensive experience using AWS:\n\n\t• Basic understanding of the machine learning workflow \n\n\t• Basic understanding of linear regression, logistic regression, neural network \n\n\t• Basic understanding of model training and testing processes \n\n\t• Basic knowledge of accuracy, precision, recall, and mean square error (MSE) \n\n\t• SQL and Data Modelling for the Web",
+},
+{
+  degree: "BSc. in Geophysics",
+  school: "Federal University of Petroleum Resources, Effurun, Nigeria.",
+  timeline: { "from": "Mar 2014", "to": "Apr 2018" },
+  summary: "• Graduated with Second Class Honours",
+},
 ];
 
 const dataportfolio = [{
-        img: "https://res.cloudinary.com/dypv9yccn/image/upload/e_grayscale/v1669622525/portfolio/1_ZtO60QvYVzN3rU0VuSSU1g_af9twi.jpg",
-        desctiption: "Data cleaning with Python",
-        link: "https://link.medium.com/zLln2XkwNub",
-        categories: {
-            nodes: [
-              {
-                name: 'Development',
-              },
-            ],
-          },
-    },
-    {
-        img: "https://picsum.photos/400/800/?grayscale",
-        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-        categories: {
-            nodes: [
-              {
-                name: 'Development',
-              },
-            ],
-          },
-    },
-    {
-        img: "https://picsum.photos/400/?grayscale",
-        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-        categories: {
-            nodes: [
-              {
-                name: 'Development',
-              },
-            ],
-          },
-    },
-    {
-        img: "https://res.cloudinary.com/dypv9yccn/image/upload/v1669623868/portfolio/Fyuur_hgpsma.jpg",
-        desctiption: "Fyuur",
-        link: "https://github.com/Egbetimmy/Fyuur-musical-app",
-        categories: {
-            nodes: [
-              {
-                name: 'Development',
-              },
-            ],
-          },
-    },
-    {
-        img: "https://picsum.photos/400/300/?grayscale",
-        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-        categories: {
-            nodes: [
-              {
-                name: 'Development',
-              },
-            ],
-          },
-    },
-
-    {
-        img: "https://picsum.photos/400/600/?grayscale",
-        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-        categories: {
-            nodes: [
-              {
-                name: 'Development',
-              },
-            ],
-          },
-    },
-    
-    {
-        img: "https://res.cloudinary.com/dypv9yccn/image/upload/e_grayscale/v1669625125/portfolio/Trivia_API_hqdtij.png",
-        desctiption: "Trivia API",
-        link: "https://github.com/Egbetimmy/Trivia-Api",
-        category:"Backend",
-    },
-    {
-        img: "https://picsum.photos/400/300/?grayscale",
-        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-        categories: {
-            nodes: [
-              {
-                name: 'Development',
-              },
-            ],
-          },
-    },
+  id: 1,
+  img: "https://res.cloudinary.com/dypv9yccn/image/upload/e_grayscale/v1669622525/portfolio/1_ZtO60QvYVzN3rU0VuSSU1g_af9twi.jpg",
+  description: "Data cleaning with Python",
+  link: "https://link.medium.com/zLln2XkwNub",
+  category: 'data_analysis',
+},
+{
+  id: 2,
+  img: "https://res.cloudinary.com/dypv9yccn/image/upload/v1669623868/portfolio/Fyuur_hgpsma.jpg",
+  description: "Fyuur",
+  link: "https://github.com/Egbetimmy/Fyuur-musical-app",
+  category: 'web_development',
+},
+{
+  id: 3,
+  img: "https://picsum.photos/400/300/?grayscale",
+  description: "Coffee Shop",
+  link: "https://github.com/Egbetimmy/Coffee-Shop",
+  category: 'web_development',
+},
+{
+  id: 4,
+  img: "https://res.cloudinary.com/dypv9yccn/image/upload/e_grayscale/v1669625125/portfolio/Trivia_API_hqdtij.png",
+  description: "Trivia API",
+  link: "https://github.com/Egbetimmy/Trivia-Api",
+  category: "web_development",
+},
+{
+  id: 5,
+  img: "https://picsum.photos/400/300/?grayscale",
+  description: "Server Deployment and Containerization",
+  link: "https://github.com/Egbetimmy/cd0157-Server-Deployment-and-Containerization",
+  category: 'web_development',
+},
+{
+  id: 6,
+  img: "https://picsum.photos/400/300/?grayscale",
+  description: "Unsupervized Machine Learning",
+  link: "https://github.com/Egbetimmy/GRIP-Internship/blob/main/Unsupervised_learning.ipynb",
+  category: 'data_science',
+},
+{
+  id: 7,
+  img: "https://picsum.photos/400/300/?grayscale",
+  description: "Web Scraping",
+  link: "https://github.com/Egbetimmy/Web-Scraping",
+  category: 'data_analysis',
+},
+{
+  id: 8,
+  img: "https://picsum.photos/400/300/?grayscale",
+  description: "Tic Tac Toe",
+  link: "https://github.com/Egbetimmy/Tic-Tac-Toe",
+  category: 'javascript',
+},
+{
+  id: 9,
+  img: "https://picsum.photos/400/300/?grayscale",
+  description: "Todo App",
+  link: "https://github.com/Egbetimmy/Todo-App",
+  category: 'javascript',
+},
+{
+  id: 10,
+  img: "https://picsum.photos/400/300/?grayscale",
+  description: "Calculator",
+  link: "https://github.com/Egbetimmy/Calculator",
+  category: 'javascript',
+},
+{
+  id: 11,
+  img: "https://picsum.photos/400/300/?grayscale",
+  description: "Customer Analysis",
+  link: "https://public.tableau.com/app/profile/egbe.timmy/viz/CustomerAnalysis_16675062924670/Dashboard1#1",
+  category: 'data_analysis',
+},
 ];
 
+
+
 const contactConfig = {
-    YOUR_EMAIL: "timmytimeyin@gmail.com",
-    YOUR_FONE: "(234)80-729-67944",
-    description: "I’m always interested in hearing about new projects and opportunities.",
-    // creat an emailjs.com account 
-    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
-    YOUR_SERVICE_ID: "service_p087iht",
-    YOUR_TEMPLATE_ID: "template_7pow8qb",
-    YOUR_USER_ID: "7DvRVF3_jGZRyJ-mD",
+  YOUR_EMAIL: "timmytimeyin@gmail.com",
+  YOUR_FONE: "(234)80-729-67944",
+  description: "I’m always interested in hearing about new projects and opportunities.",
+  // creat an emailjs.com account 
+  // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+  YOUR_SERVICE_ID: "service_p087iht",
+  YOUR_TEMPLATE_ID: "template_7pow8qb",
+  YOUR_USER_ID: "7DvRVF3_jGZRyJ-mD",
 };
 
 const socialprofils = {
-    github: "https://github.com/Egbetimmy",
-    linkedin: "https://www.linkedin.com/in/egbe-timmy-867672171/",
-    twitter: "https://twitter.com/TimmyEgbe",
+  github: "https://github.com/Egbetimmy",
+  linkedin: "https://www.linkedin.com/in/egbe-timmy-867672171/",
+  twitter: "https://twitter.com/TimmyEgbe",
 };
 export {
-    meta,
-    dataabout,
-    dataportfolio,
-    workexperience,
-    skills,
-    Education,
-    introdata,
-    contactConfig,
-    socialprofils,
-    logotext,
+  meta,
+  dataabout,
+  dataportfolio,
+  workexperience,
+  skills,
+  Education,
+  introdata,
+  contactConfig,
+  socialprofils,
+  logotext,
 };
--- src/pages/portfolio/index.js
+++ src/pages/portfolio/index.js
@@ -1,151 +1,80 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.css";
-import ReactDOM from 'react-dom';
+import { Helmet, HelmetProvider } from "react-helmet-async";
+import { Container, Row, Col } from "react-bootstrap";
+import { dataportfolio, meta } from "../../content_option";
 
 export const Portfolio = () => {
+    const [data, setData] = useState(dataportfolio)
+    const filterResult = (catItem) => {
+        const result = dataportfolio.filter((curData) => {
+            return curData.category === catItem;
+        });
+        setData(result);
 
-/* Get an array of photos with parameters */
-
-    var PHOTODATA = [{
-        id: 1,
-        category: 'fruits',
-        imageUrl: 'http://polzavred.ru/wp-content/uploads/klubnika-poleznie-svoistva.jpg'
-    },{
-        id: 2,
-        category: 'fruits',
-        imageUrl: 'https://blistogreen.com.ua/180-home_default/grusha-noyabrskaya-11-13-m.jpg'
-    },
-    {
-        id: 3,
-        category: 'fruits',
-        imageUrl: 'http://3.imimg.com/data3/RD/MF/MY-8642081/juicy-red-apples-250x250.jpg'
-    },{
-        id: 4,
-        category: 'vegetables',
-        imageUrl: 'http://portamur.ru.opt-images.1c-bitrix-cdn.ru/upload/iblock/60f/kartofell.jpg?146035093210522'
-    },{
-        id: 5,
-        category: 'vegetables',
-        imageUrl: 'http://www.slim.ru/news_img/2050img2.png'
-    },{
-        id: 6,
-        category: 'another',
-        imageUrl: 'http://clodo.infoniac.ru/iblock/9c4/9c4b4b7fa55c63d3e4aea77c3f869565/662156de618b3f2010a499c5947b6b19.jpg'
-    }];
-
-    /*Create a component of the filter panel*/
-
-    var FilterPanel = React.createClass({
-
-    render: function(){
+    }
     return (
-        <a onClick={this.props.onClick}>{this.props.category}</a>
+        <>
+            <HelmetProvider>
+                <Container className="About-header">
+                    <Helmet>
+                        <meta charSet="utf-8" />
+                        <title> Portfolio | {meta.title} </title>{" "}
+                        <meta name="description" content={meta.description} />
+                    </Helmet>
 
+                    <Row className="mb-5 mt-3">
+                        <Col lg="8">
+                            <h1 className="display-4 mb-4"> Portfolio </h1>{" "}
+                            <hr className="t_border my-4 ml-0 text-left" />
+                        </Col>
+                    </Row>
+                    <div className="col mt-5 mx-2" >
+                        <Row className="row-md-3 mb-4">
+                            <Col>
+                                <button className="btn btn-primary w-100" onClick={() => setData(dataportfolio)}>All</button>
+                            </Col>
+                            <Col>
+                                <button className="btn btn-primary w-100" onClick={() => filterResult('web_development')}>Web Development</button>
+                            </Col>
+                            <Col>
+                                <button className="btn btn-primary w-100" onClick={() => filterResult('data_science')}>Data Science</button>
+                            </Col>
+                            <Col>
+                                <button className="btn btn-primary w-100" onClick={() => filterResult('data_analysis')}>Data Analysis</button>
+                            </Col>
+                            <Col>
+                                <button className="btn btn-primary w-100" onClick={() => filterResult('javascript')}>Javascript</button>
+                            </Col>
+                        </Row>
+                        <div>
+                            <div className="row">
+                                {data.map((values) => {
+                                    const { id, img, description, link } = values;
+                                    return (
+                                        <>
+                                            <div className="col-md-4 mb-4" key={id}>
+                                                <div className="card">
+                                                    <img src={img} className="card-img-top" alt="..." />
+                                                    <div className="card-body">
+                                                        <h5 className="card-title">{description}</h5>
+
+                                                    </div>
+
+                                                </div>
+                                                <a href={link} className="btn btn-primary">view project</a>
+                                            </div>
+                                        </>
+                                    )
+                                })}
+
+                            </div>
+                        </div>
+                    </div>
+                </Container>
+            </HelmetProvider>
+
+        </>
     )
-    }
 
-    });
-
-
-
-    {/*Create a layout component for one photo*/}
-
-    var Photo =  React.createClass({
-
-    render: function(){
-    return (
-            <div className="photo-container" data-category={this.props.category} ><img src={this.props.imageUrl} /></div>
-        );
-    }
-
-
-    });
-
-
-
-
-
-
-
-    /*Create a final collection of photos in photoGallery*/
-
-    var PhotoGallery = React.createClass({
-
-    getInitialState: function() {
-            return {
-                displayedCategories: PHOTODATA,
-                active: false
-            };
-        },
-    toggleActive: function() {
-    this.setState({
-        active: !this.state.active
-    });
-    },
-
-    selectCategory: function(element){
-    console.log('element is: '+ element);
-    var categoryName = element.toLowerCase();
-            var displayedCategories = PHOTODATA.filter(function(el) {
-                
-                var searchValue = el.category.toLowerCase();
-                return searchValue.indexOf(categoryName) !== -1;
-            });
-
-            this.setState({
-                displayedCategories: displayedCategories
-
-            });
-
-    },
-    resetFilter: function(element){
-    this.setState({
-                displayedCategories: PHOTODATA
-
-            });
-    },
-
-
-
-    render: function(){
-    var buttonClass = this.state.active ? 'active' : '';
-    var categoryToSelect = this.selectCategory;
-    var getUniqueCategories=[];
-    PHOTODATA.forEach(function(el){
-        if(getUniqueCategories.indexOf(el.category) === -1 ) getUniqueCategories.push(el.category);
-    })
-
-    return (
-        <div className="overlay-photogallery">
-            <div className="filter-panel">
-                {
-                    getUniqueCategories.map(function(el,i){
-                        var boundClick = categoryToSelect.bind(null,el);
-                        return <FilterPanel onClick={boundClick} category={el} key={i} />
-                    })
-
-                }		
-                <a className="resetBtn" onClick={this.resetFilter}> Reset Filter </a>
-            </div>
-            <div className="PhotoGallery">
-                {
-
-                    this.state.displayedCategories.map(function(el){
-                                return <Photo key={el.id} imageUrl={el.imageUrl} category={el.category} />
-                            })
-
-                }
-            </div>
-        </div>
-        );
-
-
-    }
-
-
-
-    });
-
-    ReactDOM.render(<PhotoGallery />, document.getElementById('main'));
-
-}
+};
